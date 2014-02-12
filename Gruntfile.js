6c3282827936282064f1c5b6d07b2014a62790e2
François de Campredon <francois.de.campredon@gmail.com>
2014-02-12 22:55:26 +0100

    externalize istanbulify

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
         }
     });
     
-    var istanbulify = require('./grunt-utils/istanbulify');
+    var istanbulify = require('istanbulify');
     
     grunt.initConfig({
         source: ['src/declarations/*.d.ts', 'src/commons/**/*.ts', 'src/main/**/*.ts'],
