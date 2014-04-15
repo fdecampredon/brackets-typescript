4e9a5b017a0a226999d1ab4a67f1aff1662b1622
François de Campredon <francois.de.campredon@gmail.com>
2014-04-15 13:42:10 +0200

    allow reset in promise queue

--- src/commons/promiseQueue.ts
+++ src/commons/promiseQueue.ts
@@ -4,6 +4,7 @@ class PromiseQueue {
     
     private promise: Promise<any>;
     private initializer: (result: any) => any;
+    private initialized: boolean = false;
     
     constructor() {
         this.promise = new Promise(resolve => {
@@ -13,11 +14,15 @@ class PromiseQueue {
     
     init<T>(val: Promise<T>):Promise<T>
     init<T>(val: T): Promise<T> {
-        this.initializer(val);
-        return this.promise;
+        if (this.initialized) {
+            this.promise = Promise.cast(val);
+        } else {
+            this.initialized = true;
+            this.initializer(val);
+            return this.promise; 
+        }
     }
     
-   
     then<T>(action: () => Promise<T>):Promise<T>
     then<T>(action: () => T): Promise<T>
     then(action: () => void): Promise<void> {
