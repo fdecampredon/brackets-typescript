84cf9eae7af0746a09e1926189edb99f26a13287
François de Campredon <francois.de.campredon@gmail.com>
2014-04-16 11:48:11 +0200

    Simplify the language service host and add JSDoc

--- src/ts-worker/languageServiceHost.ts
+++ src/ts-worker/languageServiceHost.ts
@@ -1,4 +1,4 @@
-//   Copyright 2013 François de Campredon
+//   Copyright 2013-2014 François de Campredon
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -19,24 +19,56 @@ import collections = require('../commons/collections');
 import path = require('path');
 import utils = require('../commons/utils');
 
+/**
+ * ILanguage Service host implementation
+ */
 class LanguageServiceHost extends logger.LogingClass implements TypeScript.Services.ILanguageServiceHost {
+    
+    /**
+     * compilationSettings
+     */
     private compilationSettings: TypeScript.CompilationSettings;
   
+    /**
+     * a map associating file absolute path to ScriptInfo
+     */
     private fileNameToScript = new collections.StringMap<ScriptInfo>();
     
+    /**
+     * add a script to the host
+     * 
+     * @param fileName the absolute path of the file
+     * @param content the file content
+     */
     addScript(fileName: string, content:string) {
         var script = new ScriptInfo(fileName, content);
         this.fileNameToScript.set(fileName, script);
     }
 
+    /**
+     * remove a script from the host
+     * 
+     * @param fileName the absolute path of the file
+     */
     removeScript(fileName: string) {
         this.fileNameToScript.delete(fileName);
     }
     
+    /**
+     * remove all script from the host
+     * 
+     * @param fileName the absolute path of the file
+     */
     removeAll(): void {
         this.fileNameToScript.clear();
     }
 
+    /**
+     * update a script
+     * 
+     * @param fileName the absolute path of the file
+     * @param content the new file content
+     */
     updateScript(fileName: string, content: string) {
         var script = this.fileNameToScript.get(fileName);
         if (script) {
@@ -46,6 +78,14 @@ class LanguageServiceHost extends logger.LogingClass implements TypeScript.Servi
         throw new Error("No script with name '" + fileName + "'");
     }
 
+    /**
+     * edit a script
+     * 
+     * @param fileName the absolute path of the file
+     * @param minChar the index in the file content where the edition begins
+     * @param limChar the index  in the file content where the edition ends
+     * @param newText the text inserted
+     */
     editScript(fileName: string, minChar: number, limChar: number, newText: string) {
         var script = this.fileNameToScript.get(fileName);
         if (script) {
@@ -56,6 +96,12 @@ class LanguageServiceHost extends logger.LogingClass implements TypeScript.Servi
         throw new Error("No script with name '" + fileName + "'");
     }
     
+    /**
+     * set 'open' status of a script
+     * 
+     * @param fileName the absolute path of the file
+     * @param isOpen open status
+     */
     setScriptIsOpen(fileName: string, isOpen: boolean) {
         var script = this.fileNameToScript.get(fileName);
         if (script) {
@@ -66,10 +112,20 @@ class LanguageServiceHost extends logger.LogingClass implements TypeScript.Servi
         throw new Error("No script with name '" + fileName + "'");
     }
     
+    /**
+     * the the language service host compilation settings
+     * 
+     * @param the settings to be applied to the host
+     */
     setCompilationSettings(settings: TypeScript.CompilationSettings ): void{
-        this.compilationSettings = utils.clone(settings);
+        this.compilationSettings = Object.freeze(utils.clone(settings));
     }
     
+    /**
+     * retrieve the content of a given script
+     * 
+     * @param fileName the absolute path of the file
+     */
     getScriptContent(fileName: string): string {
         var script = this.fileNameToScript.get(fileName);
         if (script) {
@@ -78,7 +134,37 @@ class LanguageServiceHost extends logger.LogingClass implements TypeScript.Servi
         return null;
     }
     
+    /**
+     * return an index from a positon in line/char
+     * 
+     * @param path the path of the file
+     * @param position the position
+     */
+    getIndexFromPos(fileName: string, position: CodeMirror.Position): number {
+        var script = this.fileNameToScript.get(fileName);
+        if (script) {
+            return script.getPositionFromLine(position.line, position.ch)
+        }
+        return -1;
+    }
     
+    
+    /**
+     * return a positon in line/char from an index
+     * @param path the path of the file
+     * @param index the index
+     */
+    indexToPosition(fileName: string, index: number): CodeMirror.Position {
+        var script = this.fileNameToScript.get(fileName);
+        if (script) {
+            var tsPosition = script.getLineAndColForPositon(index);
+            return {
+                ch: tsPosition.character,
+                line: tsPosition.line
+            }
+        }
+        return null;
+    }
    
 
     //////////////////////////////////////////////////////////////////////
@@ -148,41 +234,12 @@ class LanguageServiceHost extends logger.LogingClass implements TypeScript.Servi
     getParentDirectory(fileName: string): string {
         return path.dirname(fileName);
     }
-
-    /**
-     * return an index from a positon in line/char
-     * @param path the path of the file
-     * @param position the position
-     */
-    getIndexFromPos(fileName: string, position: CodeMirror.Position): number {
-        var script = this.fileNameToScript.get(fileName);
-        if (script) {
-            return script.getPositionFromLine(position.line, position.ch)
-        }
-        return -1;
-    }
-    
-    
-    /**
-     * return a positon in line/char from an index
-     * @param path the path of the file
-     * @param index the index
-     */
-    indexToPosition(fileName: string, index: number): CodeMirror.Position {
-        var script = this.fileNameToScript.get(fileName);
-        if (script) {
-            var tsPosition = script.getLineAndColForPositon(index);
-            return {
-                ch: tsPosition.character,
-                line: tsPosition.line
-            }
-        }
-        return null;
-    }
 }
 
 
-
+/**
+ * Manage a script in the language service host
+ */
 class ScriptInfo {
     version: number = 1;
     editRanges: TypeScript.TextChangeRange[] = [];
@@ -193,6 +250,12 @@ class ScriptInfo {
     byteOrderMark: TypeScript.ByteOrderMark;
     
 
+    /**
+     * @param fileName the absolute path of the file
+     * @param content the content of the file
+     * @param isOpen the open status of the script
+     * @param byteOrderMark
+     */
     constructor(fileName: string, content: string, isOpen = false, byteOrderMark: TypeScript.ByteOrderMark = TypeScript.ByteOrderMark.None) {
         this.fileName = fileName;
         this.content = content;
@@ -200,20 +263,31 @@ class ScriptInfo {
         this.byteOrderMark = byteOrderMark;
         this.setContent(content);
     }
+    
+   
+    
 
-    private setContent(content: string): void {
-        this.content = content;
-        this.lineMap = TypeScript.LineMap1.fromString(content);
-    }
-
+    /**
+     * update the content of the script
+     * 
+     * @param newContent the new script content
+     */
     updateContent(newContent: string): void {
         if (newContent !== this.content) {
-            this.content = newContent;
+            this.setContent(newContent);
             this.editRanges = []
             this.version++;
         }
     }
 
+
+    /**
+     * edit the script content
+     * 
+     * @param minChar the index in the file content where the edition begins
+     * @param limChar the index  in the file content where the edition ends
+     * @param newText the text inserted
+     */
     editContent(minChar: number, limChar: number, newText: string): void {
         // Apply edits
         var prefix = this.content.substring(0, minChar);
@@ -229,6 +303,11 @@ class ScriptInfo {
         this.version++;
     }
 
+    /**
+     * return edit range since a given version
+     * 
+     * @param version
+     */
     getTextChangeRangeSinceVersion(version: number): TypeScript.TextChangeRange {
         if (this.version === version) {
             return TypeScript.TextChangeRange.unchanged;
@@ -243,42 +322,59 @@ class ScriptInfo {
     }
      
      
-    getPositionFromLine(line :number, col:number) {
-        return this.lineMap.getPosition(line, col);
+    /**
+     * return an index position from line an character position
+     * 
+     * @param line line number
+     * @param character charecter poisiton in the line
+     */
+    getPositionFromLine(line: number, character: number) {
+        return this.lineMap.getPosition(line, character);
     }
      
+    /**
+     * return line and chararacter position from index position
+     * 
+     * @param position
+     */
     getLineAndColForPositon(position: number) {
         var lineAndChar = { line: -1, character: -1};
         this.lineMap.fillLineAndCharacterFromPosition(position, lineAndChar)
         return lineAndChar;
     }
+    
+    
+    /**
+     * set the script content
+     */
+    private setContent(content: string): void {
+        this.content = content;
+        this.lineMap = TypeScript.LineMap1.fromString(content);
+    }
 }
 
  
-
+/**
+ * ScriptSnapshot implementation
+ * Simply a proxy over ScriptInfo
+ */
 class ScriptSnapshot implements TypeScript.IScriptSnapshot {
-    private lineMap: TypeScript.LineMap = null;
-    private textSnapshot: string;
     private scriptInfo: ScriptInfo;
 
     constructor(scriptInfo: ScriptInfo) {
         this.scriptInfo = scriptInfo;
-        this.textSnapshot = scriptInfo.content;
     }
 
     getText(start: number, end: number): string {
-        return this.textSnapshot.substring(start, end);
+        return this.scriptInfo.content.substring(start, end);
     }
 
     getLength(): number {
-        return this.textSnapshot.length;
+        return this.scriptInfo.content.length;
     }
 
     getLineStartPositions(): number[] {
-        if (this.lineMap === null) {
-            this.lineMap = TypeScript.LineMap1.fromString(this.textSnapshot);
-        }
-        return this.lineMap.lineStarts();
+        return this.scriptInfo.lineMap.lineStarts();
     }
 
     getTextChangeRangeSinceVersion(scriptVersion: number): TypeScript.TextChangeRange {
